5a43bcfc5a0a19054575873f8c1439edb7c14be5
Maximilian Welsch <maxiwelsch@posteo.de>
2022-01-26 11:27:38 +0100

    write basic perfomance test for raspi

--- test/performance/navigation_list_buildup.py
+++ test/performance/navigation_list_buildup.py
@@ -1,4 +1,5 @@
-from subprocess import run
+from subprocess import run, PIPE
+from time import time
 
 from krv2.music_collection import Database, Navigation
 
@@ -7,9 +8,17 @@ TEST_DB_PATH = "test/test_mishmash.db"
 
 def test_db_buildup():
     cmd = f"mishmash -D sqllite:///{TEST_DB_PATH} sync ~/Music"
-    run(cmd.split())
+    begin = time()
+    p = run(cmd.split(), stdout=PIPE)
+    for line in p.stdout:
+        pass
+    duration = time() - begin
+    print(f"scan of {TEST_DB_PATH} took {duration:.1f} seconds")
 
 
 def test_list_buildup_speed():
     db = Database({"path": TEST_DB_PATH})
+    begin = time()
     nav = Navigation({}, db)
+    duration = time() - begin
+    print(f"listing all artists of {TEST_DB_PATH} took {duration:.1f} seconds")
